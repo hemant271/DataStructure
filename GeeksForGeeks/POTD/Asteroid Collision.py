4c96939008f4634ec344bcea5c0265ea4e14d318
Hemant <patidarhemant27@gmail.com>
2022-12-29 01:54:48 +0530

    2022/12/29 - Time: O(N), Space: O(N)

---
+++ GeeksForGeeks/POTD/Asteroid Collision.py
@@ -0,0 +1,34 @@
+#User function Template for python3
+
+class Solution:
+    def asteroidCollision(self, N, asteroids):
+        
+        ans = []
+        for new in asteroids:
+            while ans and new < 0 < ans[-1]:
+                if ans[-1] < -new:
+                    ans.pop()
+                    continue
+                elif ans[-1] == -new:
+                    ans.pop()
+                break
+            else:
+                ans.append(new)
+        return ans
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        asteroids = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.asteroidCollision(N, asteroids)
+        for val in res:
+            print(val, end = ' ')
+        print()
+# } Driver Code Ends
